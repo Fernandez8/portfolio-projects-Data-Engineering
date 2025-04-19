ca8bd3ef70e074cdc325b758279302e803327fda
Fernandez8 <fernandeznguimeya@gmail.com>
2025-04-19 10:37:59 +0200

    commit

--- project-2-Data-Engineering-OS/data/consummer.py
+++ project-2-Data-Engineering-OS/data/consummer.py
@@ -27,25 +27,25 @@ connection_properties = {
 def get_schema():
     return StructType([
         StructField("DateTime", StringType(), True),
-        StructField("oil_temperature", StringType(), True),
-        StructField("oil_pressure", StringType(), True),
-        StructField("engine_power", StringType(), True),
-        StructField("engine_speed", StringType(), True),
-        StructField("engine_vibrations", StringType(), True)
+        StructField("temperature_huile", StringType(), True),
+        StructField("pression_huile", StringType(), True),
+        StructField("puissance_moteur", StringType(), True),
+        StructField("motor_speed", StringType(), True),
+        StructField("Vibrations_moteur", StringType(), True)
     ])
 
 def start_streaming():
     try:
-        # ## Spark, Kafka + Postgres
+        # ## Spark  Kafka + Postgres
         spark = SparkSession.builder \
             .appName("SparkStructuredStreaming") \
             .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.2,org.postgresql:postgresql:42.2.22") \
             .getOrCreate()
 
         spark.sparkContext.setLogLevel("ERROR")
-        logger.info("Spark session created")
+        logger.info("Spark session créée")
 
-        # ## read Kafka topic
+        # ##  Kafka topic
         df = spark.readStream \
             .format("kafka") \
             .option("kafka.bootstrap.servers", "kafka1:19092") \
@@ -53,32 +53,32 @@ def start_streaming():
             .option("startingOffsets", "earliest") \
             .load()
 
-        ### Parsing JSON + cast
+        ###  Parsing JSON + cast
         schema = get_schema()
-    
         parsed_df = df.selectExpr("CAST(value AS STRING) as json") \
             .select(from_json(col("json"), schema).alias("data")) \
             .select("data.*") \
             .select(
                 to_timestamp(col("DateTime"), "yy-MM-dd HH:mm:ss").alias("DATETIME"),
-                spark_round(col("oil_temperature").cast("float"), 2).alias("oil_temperature"),
-                spark_round(col("oil_pressure").cast("float"), 2).alias("oil_pressure"),
-                spark_round(col("engine_power").cast("float"), 2).alias("engine_power"),
-                spark_round(col("engine_speed").cast("float"), 2).alias("engine_speed")
+                spark_round(col("temperature_huile").cast("float"), 2).alias("temperature_huile"),
+                spark_round(col("pression_huile").cast("float"), 2).alias("pression_huile"),
+                spark_round(col("puissance_moteur").cast("float"), 2).alias("puissance_moteur"),
+                spark_round(col("motor_speed").cast("float"), 2).alias("motor_speed")
             )
 
-        ### write in PostgreSQL
+        ### write in  PostgreSQL
         def write_to_postgres(batch_df, batch_id):
             try:
+
                 batch_df.write.jdbc(
                     url=jdbc_url,
                     table="DATA_FLOW",
                     mode="append",
                     properties=connection_properties
                 )
-                logger.info(f"Batch {batch_id} written in PostgreSQL")
+                logger.info(f" Batch {batch_id} written in PostgreSQL")
             except Exception as e:
-                logger.error(f"Error when writing batch {batch_id}: {e}")
+                logger.error(f"Error when writing batch {batch_id} : {e}")
                 traceback.print_exc()
 
         # ### Streaming
@@ -90,7 +90,7 @@ def start_streaming():
         query.awaitTermination()
 
     except Exception as e:
-        logger.error(f"Error in pipeline: {e}")
+        logger.error(f"Erreur dans le pipeline : {e}")
         traceback.print_exc()
 
 if __name__ == "__main__":
--- project-2-Data-Engineering-OS/data/sensor_api.py
+++ project-2-Data-Engineering-OS/data/sensor_api.py
@@ -8,38 +8,37 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-def add_noise(value, noise_min, noise_max):
-    return round(value + random.uniform(noise_min, noise_max), 2)
+def ajouter_bruit(valeur, bruit_min, bruit_max):
+    return round(valeur + random.uniform(bruit_min, bruit_max), 2)
 
 @app.route('/sensordata')
 def get_sensor_data():
     timestamp = datetime.now().strftime("%y-%m-%d %H:%M:%S")
 
     #### 
-    oil_temperature_base = random.uniform(75.0, 90.0)
-    oil_pressure_base = random.uniform(2.0, 3.0)
-    engine_power_base = random.uniform(150.0, 350.0)
-    engine_speed_base = random.uniform(1450, 1550)
-    engine_vibrations_base = random.uniform(0.3, 0.7)
+    temperature_huile_base = random.uniform(75.0, 90.0)
+    pression_huile_base = random.uniform(2.0, 3.0)
+    puissance_moteur_base = random.uniform(150.0, 350.0)
+    motor_speed_base = random.uniform(1450, 1550)
+    vibrations_moteur_base = random.uniform(0.3, 0.7)
 
-    # Adding slight noise
-    oil_temperature = add_noise(oil_temperature_base, -2, 2)
-    oil_pressure = add_noise(oil_pressure_base, -0.2, 0.2)
-    engine_power = add_noise(engine_power_base, -10, 10)
-    engine_speed = round(add_noise(engine_speed_base, -30, 30), 0)
-    engine_vibrations = add_noise(engine_vibrations_base, -0.1, 0.1)
+    # #### Leichtes Rauschen hinzugefügt
+    temperature_huile = ajouter_bruit(temperature_huile_base, -2, 2)
+    pression_huile = ajouter_bruit(pression_huile_base, -0.2, 0.2)
+    puissance_moteur = ajouter_bruit(puissance_moteur_base, -10, 10)
+    motor_speed = round(ajouter_bruit(motor_speed_base, -30, 30), 0)
+    vibrations_moteur = ajouter_bruit(vibrations_moteur_base, -0.1, 0.1)
 
     response = {
-        "timestamp": timestamp,
-        "oil_temperature": oil_temperature,
-        "oil_pressure": oil_pressure,
-        "engine_power": engine_power,
-        "engine_speed": engine_speed,
-        "engine_vibrations": engine_vibrations,
+        "DateTime": timestamp,
+        "temperature_huile": temperature_huile,
+        "pression_huile": pression_huile,
+        "puissance_moteur": puissance_moteur,
+        "motor_speed": motor_speed,
+        "vibrations_moteur": vibrations_moteur,
     }
 
     return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3030)
-
